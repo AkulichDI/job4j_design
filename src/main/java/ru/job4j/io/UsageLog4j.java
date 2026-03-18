cb464ecfeaea0f820ce839eba0d44c064595f271
Gerik228 <dimaakulichd6618@gmail.com>
2026-03-18 12:08:47 +1000

    2. Simple Loggin Facade 4 Java.

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -9,11 +9,33 @@ public class UsageLog4j {
 
     private static final Logger LOG  = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
+
+    public void save(String name) {
+        LOG.info("Start saving user: {}", name);
+        try {
+            // логика сохранения
+            LOG.info("User saved successfully: {}", name);
+        } catch (Exception e) {
+            LOG.error("Error while saving user: {}", name, e);
+        }
+    }
+
+
     public static void main(String[] args) {
         LOG.trace("Trace Message1");
         LOG.debug("DEBUG message1");
         LOG.info("INFO message1");
         LOG.warn("WARN message1");
         LOG.error("ERROR message1");
+
+
+        var user = new UsageLog4j();
+
+        user.save("Олег");
+
+
     }
+
+
 }
+
