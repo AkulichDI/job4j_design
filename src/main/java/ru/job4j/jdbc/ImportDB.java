b445b26e19fc59a7442fde8179e65c9d0b43f760
Gerik228 <dimaakulichd6618@gmail.com>
2026-04-08 12:24:03 +1000

    0.2. PreparedStatement [#379307 #233978] hw

--- src/main/java/ru/job4j/jdbc/ImportDB.java
+++ src/main/java/ru/job4j/jdbc/ImportDB.java
@@ -1,10 +1,7 @@
 package ru.job4j.jdbc;
 
 import java.io.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.LongSummaryStatistics;
@@ -27,13 +24,22 @@ public class ImportDB {
 
         try(BufferedReader reader = new BufferedReader(new FileReader(dump))){
             //TODO реализовать логику чтения данных
+            reader.lines();
+            String line;
+            while (  (line = reader.readLine()) != null){
 
+                String first = line.split(";")[0];
+                String second = line.split(";")[1];
 
+                if ( first.isBlank() && second.isBlank() ){
 
+                    throw new IllegalArgumentException();
 
+                }
 
+                users.add(new User(first,second));
 
-
+            }
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -65,6 +71,57 @@ public class ImportDB {
     }
 
 
+    public void initTableUsers() throws SQLException {
+
+        try ( Connection connection = DriverManager.getConnection(
+                config.getProperty("jdbc.url"),
+                config.getProperty("jdbc.username"),
+                config.getProperty("jdbc.password")
+        )){
+
+            try( Statement statement = connection.createStatement()){
+
+                String sql = "CREATE TABLE IF NOT EXISTS users (" +
+                        "id SERIAL PRIMARY KEY," +
+                        "name TEXT," +
+                        "email TEXT" +
+                        ");";
+                statement.execute(sql);
+            }catch (Exception e ){
+                e.printStackTrace();
+            }
+
+
+        }
+
+
+    }
+
+
+    public void printUsers() throws SQLException {
+        try (Connection connection = DriverManager.getConnection(
+                config.getProperty("jdbc.url"),
+                config.getProperty("jdbc.username"),
+                config.getProperty("jdbc.password")
+        );
+             Statement statement = connection.createStatement();
+             ResultSet result = statement.executeQuery("SELECT id, name, email FROM users")) {
+
+            while (result.next()) {
+                int id = result.getInt("id");
+                String name = result.getString("name");
+                String email = result.getString("email");
+
+                System.out.println(id + " | " + name + " | " + email);
+            }
+        }
+    }
+
+
+
+
+
+
     public static void main(String[] args) throws IOException, SQLException {
 
 
@@ -75,8 +132,9 @@ public class ImportDB {
 
         }
         ImportDB dataBase = new ImportDB(config,"src/main/resources/dump.txt");
+        dataBase.initTableUsers();
         dataBase.save(dataBase.load());
-
+        dataBase.printUsers();
 
 
 
